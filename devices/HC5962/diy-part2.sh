770dc3c9f33508e3506734c9c21fc1faa115d92f
zlb6680773 <101322133+zlb6680773@users.noreply.github.com>
2022-03-12 19:34:37 +0800

    Update diy-part2.sh

--- devices/HC5962/diy-part2.sh
+++ devices/HC5962/diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
 sed -i "s/hostname='OpenWrt'/hostname='HIWIFI'/" package/base-files/files/bin/config_generate
