3a447fcc4486b766aa26e427f40cef5d03c68639
zlb6680773 <101322133+zlb6680773@users.noreply.github.com>
2022-03-12 19:33:41 +0800

    Update diy-part2.sh

--- devices/x86_64/diy-part2.sh
+++ devices/x86_64/diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 rm -rf package/lean/luci-theme-argon
