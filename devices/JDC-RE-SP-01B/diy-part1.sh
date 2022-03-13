6e9910b5423abbd0092fdc31e420e5d21a3cdea1
zlb6680773 <101322133+zlb6680773@users.noreply.github.com>
2022-03-13 10:36:55 +0800

    Update diy-part1.sh

--- devices/JDC-RE-SP-01B/diy-part1.sh
+++ devices/JDC-RE-SP-01B/diy-part1.sh
@@ -18,4 +18,5 @@
 cat >> feeds.conf.default <<EOF
 src-git kenzo https://github.com/kenzok8/openwrt-packages
 src-git passwall https://github.com/xiaorouji/openwrt-passwall
+src-git small https://github.com/kenzok8/small 
 EOF
