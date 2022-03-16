17f00fa52ab4d55cc3b96c6791b046a9d86c584c
zlb6680773 <101322133+zlb6680773@users.noreply.github.com>
2022-03-16 17:38:11 +0800

    Add files via upload

---
+++ devices/JDC-RE-SP-01B1/diy-part2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# Modify hostname
+sed -i "s/hostname='OpenWrt'/hostname='JDCloud'/" package/base-files/files/bin/config_generate
+
+# Modify default theme
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+
+# Add Packages
+# git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone https://github.com/MapesxGM/luci-app-tencentddns package/luci-app-tencentddns
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone git://github.com/jerrykuku/luci-app-ttnode package/luci-app-ttnode
+git clone git://github.com/gdck/luci-app-cupsd2.3.0 package/luci-app-cupsd2.3.0
+
+# Modify the number of connections
+cat >> package/base-files/files/etc/sysctl.conf <<EOF
+net.netfilter.nf_conntrack_acct=1
+net.netfilter.nf_conntrack_checksum=0
+net.netfilter.nf_conntrack_max=65535
+net.netfilter.nf_conntrack_tcp_timeout_established=7440
+net.netfilter.nf_conntrack_udp_timeout=60
+net.netfilter.nf_conntrack_udp_timeout_stream=180
+net.netfilter.nf_conntrack_helper=1
+EOF
+
+# disable bridge firewalling
+sed -i 's/=1/=0/g' package/kernel/linux/files/sysctl-br-netfilter.conf
